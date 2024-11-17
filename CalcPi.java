2ae7183d990fdc73a0a0152cc97c6a78e4c77f61
תמר פלד <Tamar-Peled>
2024-11-17 23:13:52 +0200

    CalcPi1

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,21 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+
+		int numbersOfTerms =Integer.parseInt(args[0]);
+		double sum = 0;
+		
+
+		for(int i = 0; i < numbersOfTerms; i++){
+
+			sum += Math.pow(-1, i)/(2*i +1);
+		}
+
+		double piApproximated = 4* sum;
+
+		System.out.println("pi according to Java " + Math.PI);
+		System.out.println("pi approximated      " + piApproximated );
+
+	    
 	}
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,35 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int n = Integer.parseInt(args[0]);
+		String mode = (args [1]);
+
+		
+
+		for(int seed =1; seed <= n; seed++){
+			 if (mode == "v"){
+				System.out.println("seed" + seed + ": ");
+			 }
+
+
+		while (n != 1) {
+			if (n % 2 ==0){
+				n = n/2;
+			}
+
+				else{
+					n = (n*3)+1;
+				}
+
+				System.out.print(n+ " ");
+
+			
+
+			}
+
+
+				System.out.println("Every one of the first 7 hailstone sequences reched 1.");
+
+		
 	}
 }
