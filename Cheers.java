41c5fe720e55013fd5568a540c494ee7a3f6b4c6
תמר פלד <Tamar-Peled>
2024-11-17 15:55:45 +0200

    Cheers1

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,40 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
-        }
+
+                String word = (args [0]);
+                int numberOflines = Integer.parseInt(args[1]);        
+              
+               
+                for (int i = 0; i < word.length(); i++){
+                        char currentLetter = word.toUpperCase().charAt(i);
+                        String lettersForAn = "AEFHILMNORSX";
+
+                        if (lettersForAn.indexOf(currentLetter)>= 0){
+                        System.out.println("Give me an " + currentLetter + ": " + currentLetter + "!" ); 
+                        }
+
+                        else{
+                                System.out.println("Give me a  " + currentLetter + ": " + currentLetter + "!" ); 
+                        }
+                                
+                        
+                                
+                }  
+
+                System.out.println("What does that spell? ");
+
+                for (int line = 0; line < numberOflines; line++){
+                        
+                        System.out.println(word.toUpperCase()+ "!!!");
+
+                
+
+                                  
+                        
+                }
+
+
 }
+
+        }
